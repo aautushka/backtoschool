721ca41cee0c5a063556f00cbb6046ed88eefbd0
Anton Autushka <aautushka@aautushka.office>
2018-02-15 11:17:41 +0300

    improve heap sort

--- test_heap_sort.py
+++ test_heap_sort.py
@@ -3,18 +3,36 @@ import pytest
 # given:
 # 1 9 2 8 3 7 4 6 5
 #
-# iterations:
-# 1: [1 9] 2 8 3 7 4 6 5
-# 2: [1 2 9] 8 3 7 4 6 4
-# 3: [1 2 8 9] 3 7 4 6 5
-# 4: [1 2 3 8 9] 7 4 6 5
-# 5: [1 2 3 7 8 9] 4 6 5
-# 6: [1 2 3 4 7 8 9] 6 5
-# 7: [1 2 3 4 6 7 8 9] 5
-# 8: [1 2 3 4 5 6 7 8 9]
+# heapify:
+# 1 9 [2] 8 3 [7] 4 6 5
+# [1] [9] 7 8 3 2 4 6 5 
+# 9 [1] 7 [8] 3 2 4 6 5
+# 9 8 7 [1] 3 2 4 [6] 5 
+# 9 8 7 6 3 2 4 1 5
 #
-# best O(n)
-# worst O(n^2)
+# sort:
+# 9 8 7 6 3 2 4 1 5
+# 5 8 7 6 4 2 4 1 [9] swap
+# 8 5 7 6 3 2 4 1 [9] heapify
+# 8 6 7 5 3 2 4 1 [9] heapify
+# 1 6 7 5 3 2 4 [8 9] swap 
+# 7 6 1 5 3 2 4 [8 9] heapify
+# 7 6 4 5 3 2 1 [8 9] heapify
+# 1 6 4 5 3 2 [7 8 9] swap
+# 6 1 4 5 3 2 [7 8 9] heapify
+# 6 5 4 1 3 2 [7 8 9] heapify
+# 2 5 4 1 3 [6 7 8 9] swap 
+# 5 2 4 1 3 [6 7 8 9] heapify
+# 5 3 4 1 2 [6 7 8 9] heapify
+# 2 3 4 1 [5 6 7 8 9] swap
+# 4 3 2 1 [5 6 7 8 9] heapify
+# 1 3 2 [4 5 6 7 8 9] swap
+# 3 1 2 [4 5 6 7 8 9] heapify
+# 2 1 [3 4 5 6 7 8 9] swap
+# 1 [2 3 4 5 6 7 8 9] swap
+# [1 2 3 4 5 6 7 8 9] swap
+# 
+# complexity: O(n * lg(n))
 
 def left(root):
     return root * 2 + 1
@@ -22,7 +40,7 @@ def left(root):
 def right(root):
     return root * 2 + 2
 
-def inplace_heapify(arr, root, depth):
+def heapify(arr, root, depth):
     l = left(root)
     r = right(root)
     largest = root
@@ -35,48 +53,41 @@ def inplace_heapify(arr, root, depth):
 
     if largest != root:
         arr[largest], arr[root] = arr[root], arr[largest]
-        inplace_heapify(arr, largest, depth)
+        return heapify(arr, largest, depth)
 
-def heapify(arr, root):
-    inplace_heapify(arr, root, len(arr))
     return arr
-
-def test_heapify_one():
-    assert [1] == heapify([1], 0)
-
-def test_heapify_two():
-    assert [2, 1] == heapify([2, 1], 0)
-    assert [2, 1] == heapify([1, 2], 0)
-
-def test_heapify_three():
-    assert [3, 2, 1] == heapify([3, 2, 1], 0)
-    assert [3, 1, 2] == heapify([3, 1, 2], 0)
-    assert [3, 1, 2] == heapify([2, 1, 3], 0)
-    assert [3, 2, 1] == heapify([2, 3, 1], 0)
-    assert [3, 2, 1] == heapify([1, 2, 3], 0)
-    assert [3, 1, 2] == heapify([1, 3, 2], 0)
-
-def test_heapify_four():
-    assert [4, 3, 2, 1] == heapify([4, 3, 2, 1], 0)
-    assert [3, 2, 1, 4] == heapify([1, 2, 3, 4], 0)
-
-def inplace_heap_sort(arr):
-    for i in xrange(len(arr) - 1, -1, -1):
-        inplace_heapify(arr, i, len(arr))
-    
-    print arr
-    for i in xrange(len(arr) - 1, -1, -1):
-        print "swap"
-        arr[0], arr[i] = arr[i], arr[0]
-        print arr
-        print "heap"
-        inplace_heapify(arr, 0, i)
-        print arr
-        print ""
 
 def heap_sort(arr):
-    inplace_heap_sort(arr)
+    for i in xrange(len(arr) - 1, -1, -1):
+        heapify(arr, i, len(arr))
+    
+    for i in xrange(len(arr) - 1, -1, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, 0, i)
+
     return arr
+
+def heapify_all(arr):
+    return heapify(arr, 0, len(arr))
+
+def test_heapify_one():
+    assert [1] == heapify_all([1])
+
+def test_heapify_two():
+    assert [2, 1] == heapify_all([2, 1])
+    assert [2, 1] == heapify_all([1, 2])
+
+def test_heapify_three():
+    assert [3, 2, 1] == heapify_all([3, 2, 1])
+    assert [3, 1, 2] == heapify_all([3, 1, 2])
+    assert [3, 1, 2] == heapify_all([2, 1, 3])
+    assert [3, 2, 1] == heapify_all([2, 3, 1])
+    assert [3, 2, 1] == heapify_all([1, 2, 3])
+    assert [3, 1, 2] == heapify_all([1, 3, 2])
+
+def test_heapify_four():
+    assert [4, 3, 2, 1] == heapify_all([4, 3, 2, 1])
+    assert [3, 2, 1, 4] == heapify_all([1, 2, 3, 4])
 
 def test_one():
     assert [1] == heap_sort([1])
